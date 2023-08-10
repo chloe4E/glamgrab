ae6451f62a878fd589f2cdbfe25a09b3536a8ec8
Chloé Faurie <chloe.faurie@gmail.com>
2023-08-10 08:56:55 +0100

    add a simple golang server

---
+++ server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fs:= http.FileServer(http.Dir("frontend/build"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :3000")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil{
+log.Fatal(err)
+	}
+}
