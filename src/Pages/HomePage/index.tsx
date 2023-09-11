dbf39a2cd4a952b87d00157ba1ba9da753ff2208
chloefaurie <chloe.faurie@gmail.com>
2023-09-11 13:44:58 +0200

    add basic zustand hook

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,7 +1,9 @@
+import { Typography } from "@mui/material";
 import React, { useState } from "react";
 import ButtonAppBar from "../../components/Header";
 import { ContainerWithTitle } from "../../components/PageLayout";
 import ProductList from "../../components/ProductList";
+import useGlamGrabStore from "../../store/store";
 
 const HomePage = () => {
   const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(0);
@@ -12,10 +14,16 @@ const HomePage = () => {
     setNumberOfItemsInCart(numberOfItemsInCart - 1);
   };
 
+  function BearCounter() {
+    const bears = useGlamGrabStore((state) => state.bears);
+    return <h1>{bears} around here ...</h1>;
+  }
+
   return (
     <>
       <ButtonAppBar numberOfItemsInCart={numberOfItemsInCart} />
       <ContainerWithTitle title="Articles available">
+        <Typography>{BearCounter()}</Typography>
         <ProductList
           handleAddToCart={handleAddToCart}
           handleRemoveFromCart={handleRemoveFromCart}
